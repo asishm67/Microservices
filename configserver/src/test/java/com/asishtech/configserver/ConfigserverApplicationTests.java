415bd7d7650a9803bd7a17bac7bbdc4964fa4dbf
asishkumarmondal <asishm67@gmail.com>
2026-06-08 16:34:41 +0530

    added config server

---
+++ configserver/src/test/java/com/asishtech/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asishtech.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
